2e64204203c3449abe57d4d047acd7a59644511d
nn3un <nn3un@virginia.edu>
2018-11-04 19:37:13 -0500

    Changed modifier

--- src/CSVFileWriter.java
+++ src/CSVFileWriter.java
@@ -8,7 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CSVFileWriter {
-    public static void appendToCsv(String csvFilePath, Long ms, String type, int offset, String Content) throws IOException {
+    static void appendToCsv(String csvFilePath, Long ms, String type, int offset, String Content) throws IOException {
         //A CSVPrinter to print to CSV (https://commons.apache.org/proper/commons-csv/apidocs/index.html)
         try (CSVPrinter printer = new CSVPrinter(new FileWriter(csvFilePath, true), CSVFormat.DEFAULT)) {
             printer.printRecord(ms, type, offset, Content);
